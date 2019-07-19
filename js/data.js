4f06d4ca8d90d1fdb26b69b8952dd2fc9da7dc04
LemarkNova <50534281+LemarkNova@users.noreply.github.com>
2019-07-19 18:43:07 -0400

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
+var cash = Number(localStorage.getItem('cash'));
+var vue = Number(localStorage.getItem('vue'));
+var popularité = Number(localStorage.getItem('popularité'));
+var interval = setInterval(update, 100);
 
+function update(){
+  cash = cash + 1
+  document.getElementById("cash").innerHTML = cash + '💰';
+  document.getElementById("vue").innerHTML = vue + ' 👁';
+  document.getElementById("popularité").innerHTML = popularité + ' 😃';
+}
+
+function save(){
+  localStorage.setItem('cash', cash);
+  localStorage.setItem('vue', vue);
+  localStorage.setItem('popularité', popularité);
+}
